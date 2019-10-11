80da43e697a50fdb7a250f0fbdea381fc67ebba8
Fernando Zanchetta <f@speediomail.com>
2019-10-11 12:55:11 -0300

    Created loginform

---
+++ src/views/Login.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="login">
+    <b-jumbotron header="Vue.js Chat"
+                 lead="Powered by Chatkit SDK and Bootstrap-vue"
+                 bg-variant="info"
+                 text-variant="white"
+    >
+      <p>For more information visite website</p>
+      <b-btn target="_blank" href="https://pusher.com/chatkit">More Info</b-btn>
+    </b-jumbotron>
+    <b-container>
+      <b-row>
+        <b-col lg="4" md="3"></b-col>
+        <b-col lg="4" md="6">
+          <LoginForm />
+        </b-col>
+        <b-col lg="4" md="3"></b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import LoginForm from '@/components/LoginForm.vue'
+
+export default {
+  name: 'login',
+  components: {
+    LoginForm
+  }
+}
+</script>
